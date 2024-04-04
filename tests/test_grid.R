3ebd27678fff9ed7506fd9500c36a61971350483
unknown <smonreal5@alu.ucam.edu>
2024-04-04 13:43:16 +0200

    Graficar el grid

--- tests/test_grid.R
+++ tests/test_grid.R
@@ -32,3 +32,46 @@ dmu <- list(2.5, 2)
 position <- search_dmu(grid_instance, dmu)
 position_string <- paste(position, collapse = ", ")
 cat("Posición en el grid:", position_string, "\n")
+
+# Función para graficar el grid
+plot_GRID <- function(grid_instance, data, dmu) {
+  # Extraer las coordenadas del grid
+  x_grid <- unlist(grid_instance$knot_list[[1]])
+  y_grid <- unlist(grid_instance$knot_list[[2]])
+
+  # Crear un dataframe con los puntos del grid
+  grid_df <- expand.grid(x = x_grid, y = y_grid)
+
+  # Crear un dataframe con las coordenadas de los datos
+  data_df <- data.frame(x = data$x1, y = data$x2)
+
+  # Convertir las coordenadas de la DMU en un dataframe
+  dmu_df <- data.frame(x = unlist(dmu[[1]]), y = unlist(dmu[[2]]))
+
+  # Crear un gráfico
+  p <- ggplot() +
+    # Añadir los datos
+    geom_point(data = data_df, aes(x = x, y = y), color = "blue", size = 3) +
+    # Añadir la DMU
+    geom_point(data = dmu_df, aes(x = x, y = y), color = "red", size = 3) +
+    # Añadir líneas verticales y horizontales para el grid
+    geom_vline(xintercept = x_grid, linetype = "dashed", color = "blue") +
+    geom_hline(yintercept = y_grid, linetype = "dashed", color = "blue") +
+    # Especificar límites del gráfico y ajustar la separación en los ejes x e y
+    coord_cartesian(xlim = c(0, max(x_grid)), ylim = c(0, 4)) +
+    scale_x_continuous(expand = expansion(add = c(0, 1))) +
+    scale_y_continuous(expand = expansion(add = c(0, 0.1))) +
+    # Agregar borde alrededor del gráfico
+    theme(
+      plot.margin = margin(1, 1, 1, 1, "cm"),
+      panel.border = element_rect(color = "black", linewidth = 0.5, fill = NA)
+    )
+    # Configuraciones adicionales
+    labs(x = "x1", y = "x2", title = "Grid con Inputs y DMU") +
+    theme_minimal()
+
+  return(p)
+}
+
+# Graficar el grid con los datos y la DMU
+plot_GRID(grid_instance, data, dmu)
